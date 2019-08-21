ddb4358259eef8d0fd84efcc734da47151a96158
Alice Post <ap2322@gmail.com>
2019-08-20 20:03:06 -0600

    Add menu method to potluck from iteration 4

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -4,6 +4,11 @@ class Potluck
   def initialize(date, dishes = [])
     @date = date
     @dishes = dishes
+    @menu = {
+      appetizers: [],
+      entrees: [],
+      desserts: [],
+    }
   end
 
   def add_dish(dish)
@@ -13,6 +18,33 @@ class Potluck
   def get_all_from_category(category)
     @dishes.select do |dish|
       dish.category == category
-    end 
+    end
+  end
+
+  def menu
+    self.add_appetizers_to_menu
+    self.add_entrees_to_menu
+    @menu
+  end
+
+  def add_appetizers_to_menu
+    @menu[:appetizers] = get_all_from_category(:appetizer).map do |dish|
+      dish.name
+    end
+    @menu[:appetizers] = @menu[:appetizers].sort
+  end
+
+  def add_entrees_to_menu
+    @menu[:entrees] = get_all_from_category(:entre).map do |dish|
+      dish.name
+    end
+    @menu[:entrees] = @menu[:entrees].sort
+  end
+
+  def add_desserts_to_menu
+    @menu[:desserts] = get_all_from_category(:dessert).map do |dish|
+      dish.name
+    end
+    @menu[:desserts] = @menu[:desserts].sort
   end
 end
